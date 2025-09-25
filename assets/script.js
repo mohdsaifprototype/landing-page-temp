77d8d83f8faf544c6a163be9b94b1b33d7ef928c
Mohd Saif <121762403+mohdsaifprototype@users.noreply.github.com>
2025-09-25 09:35:09 +0530

    version 0

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// Countdown to New Year
+const countDownDate = new Date("Jan 1, 2026 00:00:00").getTime();
+
+countdown();
+
+const x = setInterval(countdown, 1000);
+
+function countdown() {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = 0;
+    document.getElementById("hours").innerHTML = 0;
+    document.getElementById("minutes").innerHTML = 0;
+    document.getElementById("seconds").innerHTML = 0;
+  }
+}
